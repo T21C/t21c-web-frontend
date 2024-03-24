bb8ea2f28673047a77055ead84ff6386bbc9fbc6
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2024-03-24 09:06:00 +0700

    added spline

---
+++ client/src/components/SplineHeader.jsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+const SplineHeader = () => {
+  return (
+    <Spline scene="https://prod.spline.design/ptBhquTeXVJKNBve/scene.splinecode" />
+  );
+};
+
+export default SplineHeader;
