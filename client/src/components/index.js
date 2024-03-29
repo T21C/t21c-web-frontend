83f4c0d954fdd65c2196ad00192d069588bc9982
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2024-03-30 03:09:25 +0700

    try having inf scroll

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,6 +3,7 @@ import Card from "./Card";
 import Footer from "./Footer";
 import CompleteNav from "./CompleteNav";
 import LevelCard from "./LevelCard";
+import LevelCardRev from "./LevelCardRev";
 
 
-export { LevelCard, Navigation, Card, Footer, CompleteNav };
+export { LevelCard, Navigation, Card, Footer, CompleteNav, LevelCardRev  };
