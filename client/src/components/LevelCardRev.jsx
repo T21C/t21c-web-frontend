bad92c791356fc909b238924258398f4b27fbee6
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2024-04-03 16:06:01 +0700

    svg for no dl

--- client/src/components/LevelCardRev.jsx
+++ client/src/components/LevelCardRev.jsx
@@ -61,6 +61,12 @@ const LevelCardRev = ({ pdnDiff, pguDiff, creator, id, artist, song, clears, dl,
                 </svg>
           </a>}
 
+          {!ws && !dl &&(
+            <a href="">
+              <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#ffffff" fillRule="evenodd" d="M5.781 4.414a7 7 0 019.62 10.039l-9.62-10.04zm-1.408 1.42a7 7 0 009.549 9.964L4.373 5.836zM10 1a9 9 0 100 18 9 9 0 000-18z"></path> </g></svg>
+            </a>
+            )}
+
 
 
       </div>
