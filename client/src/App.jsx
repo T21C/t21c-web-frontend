b317f901fe28267e96099b4a119c021d4573932b
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2024-04-03 15:45:29 +0700

    lazy, switch dl icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,34 @@
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import { HomePage, LevelDetailPage, LevelPageRev } from './pages'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Suspense, lazy } from "react";
+const HomePage = lazy(() => import("./pages/HomePage.jsx"));
+const LevelDetailPage = lazy(()=> import ("./pages/LevelDetailPage.jsx"))
+const LevelPageRev = lazy(()=>import ("./pages/LevelPageRev.jsx"))
 
 function App() {
-
   return (
-    <Routes>
-      <Route path = "/" element={<HomePage/>}/>
-      <Route path = "/levels" element={<LevelPageRev/>}/>
-      <Route path='/leveldetail' element={<LevelDetailPage/>}/>
-      {/* <Route path='/leaderboard' element={<LeaderboardPage/>}/> */}
-    </Routes>
-  )
+    <Suspense
+      fallback={
+        <div
+          style={{
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "#090909",
+          }}
+        >
+          <div className="background-level"></div>
+          <div className="loader loader-level-detail"></div>
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/levels" element={<LevelPageRev />} />
+        <Route path="/leveldetail" element={<LevelDetailPage />} />
+        {/* <Route path='/leaderboard' element={<LeaderboardPage/>}/> */}
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
