a6e351e8868f3c44f8aa200fd8da0cc13b7b9a6e
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2024-04-05 01:40:10 +0700

    rewrite svg, remove logging

--- client/src/pages/LevelPageRev.jsx
+++ client/src/pages/LevelPageRev.jsx
@@ -134,10 +134,6 @@ const LevelPageRev = () => {
     return () => cancel && cancel();
   }, [query, sort, pageNumber, forceUpdate]);
 
-  useEffect(() => {
-    console.log([levelsData]);
-  }, [levelsData]);
-
   function toggleLegacyDiff() {
     setLegacyDiff(!legacyDiff);
   }
@@ -276,9 +272,9 @@ const LevelPageRev = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
             />
           </svg>
