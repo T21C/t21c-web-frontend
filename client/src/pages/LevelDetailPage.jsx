b3e41c9f24de13ce1149d4cabe111527947ac5af
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2024-04-06 18:08:21 +0700

    prettfy

--- client/src/pages/LevelDetailPage.jsx
+++ client/src/pages/LevelDetailPage.jsx
@@ -74,7 +74,6 @@ const LevelDetailPage = () => {
     const enrichPlayers = async () => {
       let storedPlayers = JSON.parse(localStorage.getItem('players')) || [];
   
-      // Only fetch and update localStorage if there's a discrepancy in player count
       if (storedPlayers.length !== passCount) {
         try {
           const playerNames = initialPlayer.map(p => p.player);
@@ -87,19 +86,17 @@ const LevelDetailPage = () => {
 
       const enrichedPlayers = initialPlayer
       .map(player => {
-        // Find additional info for each player
         const additionalInfo = storedPlayers.find(info => info.name === player.player);
         
-        // Only enrich and include the player if they are not banned
         if (additionalInfo && !additionalInfo.isBanned) {
           return {
             ...player,
-            country: additionalInfo.country, // Add country info from additionalInfo
+            country: additionalInfo.country, 
           };
         }
-        return null; // Return null for banned players or those without additional info
+        return null; 
       })
-      .filter(player => player !== null); // Filter out any players set to null (banned or without additional info)
+      .filter(player => player !== null); 
     
     setPlayer(enrichedPlayers);
     };
@@ -108,6 +105,13 @@ const LevelDetailPage = () => {
       enrichPlayers();
     }
   }, [initialPlayer, passCount]);
+
+  useEffect(() => {
+
+    const sortedPlayers = sortLeaderboard(player); 
+    setDisplayedPlayers(sortedPlayers); 
+  }, [player, leaderboardSort]);
+
 
 
   function handleSort(sort){
@@ -140,11 +144,6 @@ const LevelDetailPage = () => {
   };
   
 
-  useEffect(() => {
-
-    const sortedPlayers = sortLeaderboard(player); 
-    setDisplayedPlayers(sortedPlayers); 
-  }, [player, leaderboardSort]);
 
 
   if (res == null)
@@ -164,21 +163,13 @@ const LevelDetailPage = () => {
 
       <div className="wrapper-level wrapper-level-top">
         <div className="header">
-          <div
-            className="left"
+
+          <div className="left"
             style={{
-              backgroundImage: `url(${
-                res && res.level
-                  ? getYouTubeThumbnailUrl(res.level.vidLink, res.level.song)
-                  : "defaultImageURL"
-              })`,
-            }}
-          >
+              backgroundImage: `url(${res && res.level ? getYouTubeThumbnailUrl(res.level.vidLink, res.level.song): "defaultImageURL"})`}}>
+
             <div className="diff">
-              <img
-                src={getLevelImage(res.level.pdnDiff, res.level.pguDiff)}
-                alt=""
-              />
+              <img src={getLevelImage(res.level.pdnDiff, res.level.pguDiff)} alt="" />
             </div>
 
             <div className="title">
@@ -195,31 +186,16 @@ const LevelDetailPage = () => {
             {res.level.dlLink && (
               <a href={res.level.dlLink} target="_blank">
                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12"
-                    fill="none"
-                    stroke="#ffffff"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
+                  <path d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </a>
             )}
 
             {res.level.workshopLink && (
               <a href={res.level.workshopLink} target="_blank">
-                <svg
-                  fill="#ffffff"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg fill="#ffffff" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" >
                   <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                   <g id="SVGRepo_iconCarrier">
                     <path d="M 22 6 C 18.745659 6 16.09469 8.6041857 16.007812 11.837891 L 12.337891 17.083984 C 12.065931 17.032464 11.786701 17 11.5 17 C 10.551677 17 9.673638 17.297769 8.9472656 17.800781 L 4 15.84375 L 4 21.220703 L 7.1054688 22.449219 C 7.5429388 24.475474 9.3449541 26 11.5 26 C 13.703628 26 15.534282 24.405137 15.917969 22.310547 L 21.691406 17.984375 C 21.794183 17.989633 21.895937 18 22 18 C 25.309 18 28 15.309 28 12 C 28 8.691 25.309 6 22 6 z M 22 8 C 24.206 8 26 9.794 26 12 C 26 14.206 24.206 16 22 16 C 19.794 16 18 14.206 18 12 C 18 9.794 19.794 8 22 8 z M 22 9 A 3 3 0 0 0 22 15 A 3 3 0 0 0 22 9 z M 11.5 18 C 13.43 18 15 19.57 15 21.5 C 15 23.43 13.43 25 11.5 25 C 10.078718 25 8.8581368 24.145398 8.3105469 22.925781 L 10.580078 23.824219 C 10.882078 23.944219 11.192047 24.001953 11.498047 24.001953 C 12.494047 24.001953 13.436219 23.403875 13.824219 22.421875 C 14.333219 21.137875 13.703922 19.683781 12.419922 19.175781 L 10.142578 18.273438 C 10.560118 18.097145 11.019013 18 11.5 18 z"></path>
                   </g>
@@ -238,57 +214,44 @@ const LevelDetailPage = () => {
 
             <div className="info-item">
               <p>
-                {" "}
                 <span className="one">#1</span> Clear
               </p>
               <span className="info-desc">
-                {player.length > 0
-                  ? `${player[0].player} | ${player[0].vidUploadTime.slice(
-                      0,
-                      10
-                    )}`
+                {player.length > 0 ? 
+                `${player[0].player} | ${player[0].vidUploadTime.slice(0, 10 )}`
                   : "-"}
               </span>
             </div>
 
             <div className="info-item">
               <p>
-                {" "}
                 <span className="one">#1</span> Score
               </p>
               <span className="info-desc">
                 {player && player[highScore]
-                  ? `${player[highScore].player} | ${player[
-                      highScore
-                    ].scoreV2.toFixed(2)}`
+                  ? `${player[highScore].player} | ${player[highScore].scoreV2.toFixed(2)}`
                   : "-"}
               </span>
             </div>
 
             <div className="info-item">
               <p>
-                {" "}
                 <span className="one">#1</span> Speed
               </p>
               <span className="info-desc">
                 {player && player[highSpeed]
-                  ? `${player[highSpeed].player} | ${
-                      player[highSpeed].speed || 1
-                    }`
+                  ? `${player[highSpeed].player} | ${player[highSpeed].speed || 1}`
                   : "-"}
               </span>
             </div>
 
             <div className="info-item">
               <p>
-                {" "}
                 <span className="one">#1</span> Accuracy
               </p>
               <span className="info-desc">
                 {player && player[highAcc]
-                  ? `${player[highAcc].player} | ${(
-                      player[highAcc].accuracy * 100
-                    ).toFixed(2)}%`
+                  ? `${player[highAcc].player} | ${(player[highAcc].accuracy * 100).toFixed(2)}%`
                   : "-"}
               </span>
             </div>
@@ -314,7 +277,7 @@ const LevelDetailPage = () => {
               allowFullScreen
             ></iframe>
           :
-          <img src={getYouTubeThumbnailUrl(res.level.vidLink, res.level.song)} alt="" />
+            <img src={getYouTubeThumbnailUrl(res.level.vidLink, res.level.song)} alt="" />
           }
           </div>
         </div>
@@ -332,67 +295,37 @@ const LevelDetailPage = () => {
                 <Tooltip id="sc" place="top" noArrow>
                   Score
                 </Tooltip>
-              <svg
-                style={{
-                  backgroundColor:
-                    leaderboardSort == "TIME" ? "rgba(255, 255, 255, 0.7)" : "",
-                }}
-                data-tooltip-id = "tm"
-                value="TIME"
-                onClick={() => handleSort("TIME")}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+              data-tooltip-id = "tm"
+              value="TIME" 
+              onClick={() => handleSort("TIME")} 
+              style={{
+                backgroundColor:
+                  leaderboardSort == "TIME" ? "rgba(255, 255, 255, 0.7)" : "",
+              }}>
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></g>
+                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
-                  {" "}
-                  <path
-                    d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z"
-                    stroke="#ffffff"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                  ></path>{" "}
+                  <path d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" ></path>
                 </g>
               </svg>
 
-              <svg
+              <svg                 viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
               style={{
                 backgroundColor:
                   leaderboardSort == "ACC" ? "rgba(255, 255, 255, 0.7)" : "",
               }}
               data-tooltip-id = "ac"
               value="ACC"
-              onClick={() => handleSort("ACC")}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              onClick={() => handleSort("ACC")} >
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></g>
+                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" ></g>
                 <g id="SVGRepo_iconCarrier">
-                  {" "}
-                  <path
-                    d="M21.4143 3.29285C21.8048 3.68337 21.8048 4.31653 21.4143 4.70706L4.70718 21.4142C4.31666 21.8047 3.68349 21.8047 3.29297 21.4142L2.58586 20.7071C2.19534 20.3165 2.19534 19.6834 2.58586 19.2928L19.293 2.58574C19.6835 2.19522 20.3167 2.19522 20.7072 2.58574L21.4143 3.29285Z"
-                    fill="#ffffff"
-                  ></path>{" "}
-                  <path
-                    d="M7.50009 2.99997C5.5671 2.99997 4.00009 4.56697 4.00009 6.49997C4.00009 8.43297 5.5671 9.99997 7.50009 9.99997C9.43309 9.99997 11.0001 8.43297 11.0001 6.49997C11.0001 4.56697 9.43309 2.99997 7.50009 2.99997Z"
-                    fill="#ffffff"
-                  ></path>{" "}
-                  <path
-                    d="M16.5001 14C14.5671 14 13.0001 15.567 13.0001 17.5C13.0001 19.433 14.5671 21 16.5001 21C18.4331 21 20.0001 19.433 20.0001 17.5C20.0001 15.567 18.4331 14 16.5001 14Z"
-                    fill="#ffffff"
-                  ></path>{" "}
+
+                  <path d="M21.4143 3.29285C21.8048 3.68337 21.8048 4.31653 21.4143 4.70706L4.70718 21.4142C4.31666 21.8047 3.68349 21.8047 3.29297 21.4142L2.58586 20.7071C2.19534 20.3165 2.19534 19.6834 2.58586 19.2928L19.293 2.58574C19.6835 2.19522 20.3167 2.19522 20.7072 2.58574L21.4143 3.29285Z" fill="#ffffff" ></path>
+                  <path d="M7.50009 2.99997C5.5671 2.99997 4.00009 4.56697 4.00009 6.49997C4.00009 8.43297 5.5671 9.99997 7.50009 9.99997C9.43309 9.99997 11.0001 8.43297 11.0001 6.49997C11.0001 4.56697 9.43309 2.99997 7.50009 2.99997Z" fill="#ffffff" ></path>
+                  <path d="M16.5001 14C14.5671 14 13.0001 15.567 13.0001 17.5C13.0001 19.433 14.5671 21 16.5001 21C18.4331 21 20.0001 19.433 20.0001 17.5C20.0001 15.567 18.4331 14 16.5001 14Z" fill="#ffffff" ></path>
                 </g>
               </svg>
 
