b0eb289b4878a6531e1cfed6bd8693ab8e836d3b
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2024-11-08 12:53:19 +0200

    move out admin css

--- src/pages/AdminPage/RatingPage.jsx
+++ src/pages/AdminPage/RatingPage.jsx
@@ -1,5 +1,5 @@
 import { CompleteNav } from "../../components";
-import "./adminratingpage.css";
+import "./css/adminratingpage.css";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useTranslation } from "react-i18next";
--- src/pages/AdminPage/SubmissionManagementPage.jsx
+++ src/pages/AdminPage/SubmissionManagementPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from "../../context/AuthContext";
-import "./adminsubmissionpage.css";
+import "./css/adminsubmissionpage.css";
 import { CompleteNav } from '../../components';
 import { getVideoDetails } from '../../Repository/RemoteRepository';
 import placeholder from "../../assets/placeholder/3.png";
