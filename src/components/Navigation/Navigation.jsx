018d4a77341e9c783a7639334aca5592185c8ea4
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2024-12-27 03:50:55 +0200

    add jp preset

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -29,6 +29,7 @@ const Navigation = ({ children }) => {
     en: { display: "English", countryCode: "us", implemented: true },
     kr: { display: "한국어", countryCode: "kr", implemented: false },
     cn: { display: "中文", countryCode: "cn", implemented: false },
+    jp: { display: "日本語", countryCode: "jp", implemented: false },
     id: { display: "Bahasa Indonesia", countryCode: "id", implemented: false },
     ru: { display: "Русский", countryCode: "ru", implemented: false },
     de: { display: "Deutsch", countryCode: "de", implemented: false },
