dc0ffe3963d164f34c74ee6f69d2af7d31faec04
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-01-20 21:34:55 +0200

    rephrase warning

--- src/components/StagingModeWarning/StagingModeWarning.jsx
+++ src/components/StagingModeWarning/StagingModeWarning.jsx
@@ -9,7 +9,7 @@ const StagingModeWarning = ({ className }) => {
       <div className="warning-icon">⚠️</div>
       <div className="warning-content">
         <h4>Warning! This page is in staging mode.</h4>
-        <p>This page is currently in staging mode. Any changes you make will not be reflected on the live site, please go to <a href="https://tuforums.com">tuforums.com</a> to make changes.</p>
+        <p>This page is currently in staging mode. Any submissions you make wont go through, please go to <a href="https://tuforums.com">tuforums.com</a> to submit.</p>
       </div>
     </div>
   );
