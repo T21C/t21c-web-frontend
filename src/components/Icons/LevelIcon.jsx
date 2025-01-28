c562e9819b8445370623e9f850a896b13dce1993
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-01-29 01:04:35 +0200

    fix icon import

--- src/components/Icons/LevelIcon.jsx
+++ src/components/Icons/LevelIcon.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import levelIcon from '@/assets/icons/chart.png';
 
 export const LevelIcon = ({ size=24, ...props }) => {
   return (
-    <img {...props} width={size} height={size} src="/src/assets/icons/chart.png" alt="level-icon" />
+    <img {...props} width={size} height={size} src={levelIcon} alt="level-icon" />
   );
 };
 
