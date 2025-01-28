669cfbc9e598f171901027cc234a1807f9263e15
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-01-29 01:08:32 +0200

    footer logo fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { Link } from 'react-router-dom';
 import './footer.css';
+import logoIcon from '@/assets/tuf-logo/logo.png';
 
 const Footer = () => {
   const { t } = useTranslation('pages');
@@ -10,7 +11,7 @@ const Footer = () => {
     <footer>
       <div className="footer-content">
         <div className="footer-logo">
-          <img src="/src/assets/tuf-logo/logo.png" alt="TUF" />
+          <img src={logoIcon} alt="TUF" />
         </div>
         <div className="footer-section">
           <h4>{tHome('about.title')}</h4>
