a49ff73d1e29ce538d3bfb5a59f2fff733190443
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-05-03 01:31:03 +0300

    fix dom and FCP

--- src/pages/common/HomePage/HomePage.jsx
+++ src/pages/common/HomePage/HomePage.jsx
@@ -282,7 +282,7 @@ const HomePage = () => {
             aria-label="Health"
            >
               <span className="button-content">
-                  <svg fill="#0f0" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 316.89 316.89" xml:space="preserve" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M182.47,212.195c0-0.754,0.036-1.5,0.057-2.25H38.5c-21.229,0-38.5,17.271-38.5,38.5s17.271,38.5,38.5,38.5h199.493 C205.905,277.24,182.47,247.408,182.47,212.195z M81.5,258.445h-35c-5.514,0-10-4.486-10-10c0-5.514,4.486-10,10-10h35 c5.514,0,10,4.486,10,10C91.5,253.959,87.014,258.445,81.5,258.445z"></path> <path d="M38.5,196.945h145.476c7.114-35.78,38.743-62.837,76.581-62.837c9.54,0,18.682,1.727,27.139,4.872 c-6.697-11.378-19.066-19.035-33.196-19.035h-216c-21.229,0-38.5,17.271-38.5,38.5S17.271,196.945,38.5,196.945z M46.5,148.445h35 c5.514,0,10,4.486,10,10c0,5.514-4.486,10-10,10h-35c-5.514,0-10-4.486-10-10C36.5,152.931,40.986,148.445,46.5,148.445z"></path> <path d="M38.5,106.945h216c21.228,0,38.5-17.271,38.5-38.5s-17.272-38.5-38.5-38.5h-216c-21.229,0-38.5,17.271-38.5,38.5 S17.271,106.945,38.5,106.945z M237.5,55.695c7.03,0,12.75,5.72,12.75,12.75s-5.72,12.75-12.75,12.75 c-7.03,0-12.75-5.72-12.75-12.75S230.47,55.695,237.5,55.695z M46.5,58.445h35c5.514,0,10,4.486,10,10s-4.486,10-10,10h-35 c-5.514,0-10-4.486-10-10S40.986,58.445,46.5,58.445z"></path> <path d="M260.557,155.862c-31.112,0-56.333,25.221-56.333,56.333s25.221,56.333,56.333,56.333s56.332-25.221,56.332-56.333 S291.669,155.862,260.557,155.862z M290.653,200.261l-35.654,35.653c-0.754,0.755-1.759,1.172-2.828,1.172 c-1.069,0-2.074-0.416-2.829-1.172l-17.88-17.88c-0.756-0.755-1.172-1.76-1.172-2.828c0-1.068,0.416-2.073,1.172-2.829l3.535-3.535 c0.754-0.755,1.759-1.172,2.828-1.172c1.068,0,2.073,0.416,2.828,1.171l11.518,11.517l29.291-29.289 c0.754-0.755,1.759-1.172,2.828-1.172c1.069,0,2.074,0.417,2.829,1.172l3.535,3.535c0.755,0.754,1.172,1.759,1.172,2.828 C291.825,198.501,291.409,199.506,290.653,200.261z"></path> </g> </g></svg>
+                  <svg fill="#0f0" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 316.89 316.89" xmlSpace="preserve" stroke="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M182.47,212.195c0-0.754,0.036-1.5,0.057-2.25H38.5c-21.229,0-38.5,17.271-38.5,38.5s17.271,38.5,38.5,38.5h199.493 C205.905,277.24,182.47,247.408,182.47,212.195z M81.5,258.445h-35c-5.514,0-10-4.486-10-10c0-5.514,4.486-10,10-10h35 c5.514,0,10,4.486,10,10C91.5,253.959,87.014,258.445,81.5,258.445z"></path> <path d="M38.5,196.945h145.476c7.114-35.78,38.743-62.837,76.581-62.837c9.54,0,18.682,1.727,27.139,4.872 c-6.697-11.378-19.066-19.035-33.196-19.035h-216c-21.229,0-38.5,17.271-38.5,38.5S17.271,196.945,38.5,196.945z M46.5,148.445h35 c5.514,0,10,4.486,10,10c0,5.514-4.486,10-10,10h-35c-5.514,0-10-4.486-10-10C36.5,152.931,40.986,148.445,46.5,148.445z"></path> <path d="M38.5,106.945h216c21.228,0,38.5-17.271,38.5-38.5s-17.272-38.5-38.5-38.5h-216c-21.229,0-38.5,17.271-38.5,38.5 S17.271,106.945,38.5,106.945z M237.5,55.695c7.03,0,12.75,5.72,12.75,12.75s-5.72,12.75-12.75,12.75 c-7.03,0-12.75-5.72-12.75-12.75S230.47,55.695,237.5,55.695z M46.5,58.445h35c5.514,0,10,4.486,10,10s-4.486,10-10,10h-35 c-5.514,0-10-4.486-10-10S40.986,58.445,46.5,58.445z"></path> <path d="M260.557,155.862c-31.112,0-56.333,25.221-56.333,56.333s25.221,56.333,56.333,56.333s56.332-25.221,56.332-56.333 S291.669,155.862,260.557,155.862z M290.653,200.261l-35.654,35.653c-0.754,0.755-1.759,1.172-2.828,1.172 c-1.069,0-2.074-0.416-2.829-1.172l-17.88-17.88c-0.756-0.755-1.172-1.76-1.172-2.828c0-1.068,0.416-2.073,1.172-2.829l3.535-3.535 c0.754-0.755,1.759-1.172,2.828-1.172c1.068,0,2.073,0.416,2.828,1.171l11.518,11.517l29.291-29.289 c0.754-0.755,1.759-1.172,2.828-1.172c1.069,0,2.074,0.417,2.829,1.172l3.535,3.535c0.755,0.754,1.172,1.759,1.172,2.828 C291.825,198.501,291.409,199.506,290.653,200.261z"></path> </g> </g></svg>
                   
                 <span className="button-text">Health</span>
               </span>
@@ -290,9 +290,61 @@ const HomePage = () => {
           </Link>
         </div>
 
-        {stats == null ? 
-        (<div className="loader loader-level-page" style={{marginBottom: "400px"}}/>)
-        :(
+        {stats == null ? (
+          <div className="stats-container">
+            <section className="stats-section overview">
+              <h2>{tHome('stats.overview.title')}</h2>
+              <div className="stats-grid empty">
+                <StatCard value={0} label={tHome('stats.overview.totalLevels')} />
+                <StatCard value={0} label={tHome('stats.overview.totalPasses')} />
+                <StatCard value={0} label={tHome('stats.overview.totalPlayers')} />
+                <StatCard value={0} label={tHome('stats.overview.passesLast30Days')} />
+              </div>
+            </section>
+
+            <section className="stats-section difficulties">
+              <h2>{tHome('stats.difficulties.title')}</h2>
+              <div style={{ opacity: 0.5, transform: 'scaleY(0)', height: 0, transformOrigin: 'top' }}>
+                <div className="graph-controls">
+                  <button 
+                    className={`graph-mode ${graphMode === 'passes' ? 'active' : ''}`}
+                    onClick={() => setGraphMode('passes')}
+                  >
+                    {tHome('stats.difficulties.graphModes.passes')}
+                  </button>
+                  <button 
+                    className={`graph-mode ${graphMode === 'levels' ? 'active' : ''}`}
+                    onClick={() => setGraphMode('levels')}
+                  >
+                    {tHome('stats.difficulties.graphModes.levels')}
+                  </button>
+                </div>
+                <div style={{ height: "300px" }}></div>
+              </div>
+            </section>
+
+            <section className="stats-section discord-section">
+              <div className="discord-container">
+                <span className="discord-title">{tHome('stats.discord.title')}</span>
+                <iframe 
+                src="https://discord.com/widget?id=1024941834373439509&theme=dark" 
+                width="83.333%" 
+                height="300"
+                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                style={{
+                  position: "relative", 
+                  zIndex: "1", 
+                  transform: "scale(1.2)", 
+                  transformOrigin: "0 0", 
+                  paddingBottom: "50px",
+                  border: "none"
+                }}
+                />
+              </div>
+            </section>
+            <p style={{textAlign: "center", opacity: "0.5"}}>{tHome('stats.comingSoon')}</p>
+          </div>
+        ) : (
           <div className="stats-container">
             <section className="stats-section overview">
               <h2>{tHome('stats.overview.title')}</h2>
@@ -306,31 +358,30 @@ const HomePage = () => {
 
             <section className="stats-section difficulties">
               <h2>{tHome('stats.difficulties.title')}</h2>
-              <div className="graph-controls">
-                <button 
-                  className={`graph-mode ${graphMode === 'passes' ? 'active' : ''}`}
-                  onClick={() => {
-                    setGraphMode('passes');
-                  }}
-                >
-                  {tHome('stats.difficulties.graphModes.passes')}
-                </button>
-                <button 
-                  className={`graph-mode ${graphMode === 'levels' ? 'active' : ''}`}
-                  onClick={() => {
-                    setGraphMode('levels');
-                  }}
-                  
-                >
-                  {tHome('stats.difficulties.graphModes.levels')}
-                </button>
-              </div>
-              {Object.entries(stats.difficulties.byType).filter(([type]) => type !== 'SPECIAL').map(([type, difficulties]) => (
-                <div key={type} className="difficulty-type-section">
-                  <h3>{type}</h3>
-                  <DifficultyGraph data={difficulties} mode={graphMode} />
+              <div style={{ 
+                transformOrigin: 'top' 
+              }}>
+                <div className="graph-controls">
+                  <button 
+                    className={`graph-mode ${graphMode === 'passes' ? 'active' : ''}`}
+                    onClick={() => setGraphMode('passes')}
+                  >
+                    {tHome('stats.difficulties.graphModes.passes')}
+                  </button>
+                  <button 
+                    className={`graph-mode ${graphMode === 'levels' ? 'active' : ''}`}
+                    onClick={() => setGraphMode('levels')}
+                  >
+                    {tHome('stats.difficulties.graphModes.levels')}
+                  </button>
                 </div>
-              ))}
+                {Object.entries(stats.difficulties.byType).filter(([type]) => type !== 'SPECIAL').map(([type, difficulties]) => (
+                  <div key={type} className="difficulty-type-section">
+                    <h3>{type}</h3>
+                    <DifficultyGraph data={difficulties} mode={graphMode} />
+                  </div>
+                ))}
+              </div>
             </section>
             <section className="stats-section discord-section">
               <div className="discord-container">
