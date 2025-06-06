720424cfabf75c7e1d66d1008a3a0a7a4f3dedf6
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-06-06 19:44:41 +0300

    set 500mb limit

--- src/utils/zipUtils.js
+++ src/utils/zipUtils.js
@@ -57,7 +57,7 @@ export const decodeFilename = (encodedFilename) => {
  * @param {number} maxSizeMB - Maximum size in megabytes
  * @returns {boolean} Whether the file size is valid
  */
-export const validateZipSize = (file, maxSizeMB = 1000) => {
+export const validateZipSize = (file, maxSizeMB = 500) => {
   const maxSizeBytes = maxSizeMB * 1024 * 1024;
   return file.size <= maxSizeBytes;
 };
