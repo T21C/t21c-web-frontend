b1f3bd7acdd69fa9991c2382de0ab975bdf91728
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-09-01 15:33:35 +0300

    remove unused element

--- src/components/common/display/index.js
+++ src/components/common/display/index.js
@@ -1,13 +1,11 @@
 import AccessDenied from './AccessDenied/AccessDenied';
 import MetaTags from './MetaTags/MetaTags';
 import StagingModeWarning from './StagingModeWarning/StagingModeWarning';
-import WeeklyCurationView from './WeeklyCurationView';
 import WeeklyGallery from './WeeklyGallery/WeeklyGallery';
 
 export { 
     AccessDenied,
     MetaTags,
     StagingModeWarning,
-    WeeklyCurationView,
     WeeklyGallery
 }
