b8f6e0cd450f216a4a1b51076dc84a4ce8bed3df
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-10-08 13:51:53 +0300

    fixes !!!

--- src/components/cards/LevelCard/LevelCard.jsx
+++ src/components/cards/LevelCard/LevelCard.jsx
@@ -214,7 +214,7 @@ const LevelCard = ({
           </div>
         )}
         {packItem.isCleared ? 
-             <svg className="level-card__cleared" width="16" height="16" viewBox="0 0 24 24" fill="#4CAF50" >
+             <svg className={`level-card__cleared` + (!canEdit ? ' no-hover' : '')} width="16" height="16" viewBox="0 0 24 24" fill="#4CAF50" >
                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
              </svg>
         : null}
