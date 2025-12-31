f1b8ea179080a430150ae8f4081ce97313c98dae
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-12-31 04:35:01 +0200

    fix translation mistakes

--- src/components/popups/LevelHelpPopup/LevelHelpPopup.jsx
+++ src/components/popups/LevelHelpPopup/LevelHelpPopup.jsx
@@ -79,7 +79,7 @@ export const LevelHelpPopup = ({ onClose }) => {
               <ul>
                 <li><b><code>song:hello, artist:camellia</code></b> - {tHelp('sections.operators.examples.and')}</li>
                 <li><b><code>song=goTe | song:xnor</code></b> - {tHelp('sections.operators.examples.or')}</li>
-                <li><b><code>charter:gazizi, artist:camellia | artist=ludicin</code></b> - {tHelp('sections.operators.examples.complex')}</li>
+                <li><b><code>charter:gazizi, song:onus | artist=ludicin</code></b> - {tHelp('sections.operators.examples.complex')}</li>
                 <li><b><code>song:onus, \!nerfed</code></b> - {tHelp('sections.operators.examples.not')}</li>
               </ul>
             </div>
