6b05944d7fff95ff38539927129b0db151d6c83c
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2026-06-27 13:39:15 +0300

    more searchable pass fields

--- src/components/common/SearchHelpPopup/passSearchHelpSections.js
+++ src/components/common/SearchHelpPopup/passSearchHelpSections.js
@@ -53,11 +53,34 @@ export const PASS_SEARCH_HELP_SECTIONS = [
     introKey: 'pass.helpPopup.v2.groups.numeric.intro',
     fields: [
       { token: 'keycount', titleKey: 'pass.helpPopup.v2.fields.keycount.title', descKey: 'pass.helpPopup.v2.fields.keycount.desc' },
+      { token: 'score', titleKey: 'pass.helpPopup.v2.fields.score.title', descKey: 'pass.helpPopup.v2.fields.score.desc' },
+      { token: 'xacc', titleKey: 'pass.helpPopup.v2.fields.xacc.title', descKey: 'pass.helpPopup.v2.fields.xacc.desc' },
+      { token: 'speed', titleKey: 'pass.helpPopup.v2.fields.speed.title', descKey: 'pass.helpPopup.v2.fields.speed.desc' },
     ],
     examples: [
       'pass.helpPopup.v2.groups.numeric.examples.exact',
       'pass.helpPopup.v2.groups.numeric.examples.range',
+      'pass.helpPopup.v2.groups.numeric.examples.xaccNearPerfect',
       'pass.helpPopup.v2.groups.numeric.examples.combine',
+    ],
+  },
+  {
+    id: 'judgements',
+    titleKey: 'pass.helpPopup.v2.groups.judgements.title',
+    introKey: 'pass.helpPopup.v2.groups.judgements.intro',
+    fields: [
+      { token: 'eperfect', titleKey: 'pass.helpPopup.v2.fields.eperfect.title', descKey: 'pass.helpPopup.v2.fields.eperfect.desc' },
+      { token: 'perfect', titleKey: 'pass.helpPopup.v2.fields.perfect.title', descKey: 'pass.helpPopup.v2.fields.perfect.desc' },
+      { token: 'lperfect', titleKey: 'pass.helpPopup.v2.fields.lperfect.title', descKey: 'pass.helpPopup.v2.fields.lperfect.desc' },
+      { token: 'earlysingle', titleKey: 'pass.helpPopup.v2.fields.earlysingle.title', descKey: 'pass.helpPopup.v2.fields.earlysingle.desc' },
+      { token: 'earlydouble', titleKey: 'pass.helpPopup.v2.fields.earlydouble.title', descKey: 'pass.helpPopup.v2.fields.earlydouble.desc' },
+      { token: 'latesingle', titleKey: 'pass.helpPopup.v2.fields.latesingle.title', descKey: 'pass.helpPopup.v2.fields.latesingle.desc' },
+      { token: 'latedouble', titleKey: 'pass.helpPopup.v2.fields.latedouble.title', descKey: 'pass.helpPopup.v2.fields.latedouble.desc' },
+    ],
+    examples: [
+      'pass.helpPopup.v2.groups.judgements.examples.exactCount',
+      'pass.helpPopup.v2.groups.judgements.examples.nearPerfect',
+      'pass.helpPopup.v2.groups.judgements.examples.combine',
     ],
   },
   {
