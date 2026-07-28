7151cbe9412884fe9b00f5e7e6e6c2cff78db33f
V0W4N <vowan.denisov@gmail.com>
2026-07-29 00:16:20 +0300

    improve responsiveness on scorecard

--- src/components/cards/ScoreCard/ScoreCard.jsx
+++ src/components/cards/ScoreCard/ScoreCard.jsx
@@ -68,25 +68,37 @@ const ScoreCard = ({ scoreData, topScores = [], potentialTopScores = [], mode = 
       className={`score-card ${isPassCardMode ? 'score-card--passcard' : ''} ${isHiddenPass ? 'hidden-pass' : ''}`}
       style={cardStyle}
     >
-      <div className="img-wrapper">
-        {!isHiddenLevel && (
-          <img src={difficultyDict[scoreData.level.diffId]?.icon} referrerPolicy="no-referrer" alt="" />
-        )}
-        {isHiddenLevel && (
-          <div className="hidden-level-icon">🔒</div>
-        )}
-      </div>
-      {isPassCardMode ? (
-        <div className="score-card__info-column">
-          {scoreData.player?.id && (
-            <Link className="score-card__player-row" to={`/profile/${scoreData.player.id}`}>
-              <UserAvatar {...userAvatarUrls(scoreData.player)} className="score-card__player-avatar" />
-              <MarqueeText className="score-desc-player" as="span">
-                {scoreData.player.name}
+      <div className="score-card__row score-card__row--primary">
+        <div className="img-wrapper">
+          {!isHiddenLevel && (
+            <img src={difficultyDict[scoreData.level.diffId]?.icon} referrerPolicy="no-referrer" alt="" />
+          )}
+          {isHiddenLevel && (
+            <div className="hidden-level-icon">🔒</div>
+          )}
+        </div>
+        {isPassCardMode ? (
+          <div className="score-card__info-column">
+            {scoreData.player?.id && (
+              <Link className="score-card__player-row" to={`/profile/${scoreData.player.id}`}>
+                <UserAvatar {...userAvatarUrls(scoreData.player)} className="score-card__player-avatar" />
+                <MarqueeText className="score-desc-player" as="span">
+                  {scoreData.player.name}
+                </MarqueeText>
+              </Link>
+            )}
+            <Link className="name-wrapper" to={`/passes/${scoreData.id}`}>
+              <MarqueeText className="score-desc score-desc-song" as="p">
+                {scoreData.level.song}
+              </MarqueeText>
+              <MarqueeText className="score-exp score-exp-artist" as="p">
+                {scoreData.level.artist ?? 'Hidden level'}
               </MarqueeText>
             </Link>
-          )}
+          </div>
+        ) : (
           <Link className="name-wrapper" to={`/passes/${scoreData.id}`}>
+            <p className="score-desc-creator">{formatCreatorDisplay(scoreData.level)}</p>
             <MarqueeText className="score-desc score-desc-song" as="p">
               {scoreData.level.song}
             </MarqueeText>
@@ -94,74 +106,67 @@ const ScoreCard = ({ scoreData, topScores = [], potentialTopScores = [], mode = 
               {scoreData.level.artist ?? 'Hidden level'}
             </MarqueeText>
           </Link>
+        )}
+      </div>
+
+      <div className="score-card__row score-card__row--secondary">
+        {scoreData.isWorldsFirst && (
+          <WorldsFirstFlag variant="clear" tooltipIndex={`${scoreData.id}-clear`} className="wf-badge" />
+        )}
+        {scoreData.isWorldsFirstPP && (
+          <WorldsFirstFlag variant="pp" tooltipIndex={`${scoreData.id}-pp`} className="wf-badge" />
+        )}
+        <div className="score-wrapper">
+            <p className="score-exp">{t('score.card.labels.score')}</p>
+            <p className="score-desc">{formatScore(scoreData.scoreV2)}</p>
+            {
+            topScores.find(score => score.id === scoreData.id) ?
+            <p className="score-impact">+{formatNumber(
+              topScores.find(score => score.id === scoreData.id).impact)
+            }</p> :
+            potentialTopScores.find(score => score.id === scoreData.id) &&
+             <p className="score-impact potential"
+             data-tooltip-id="potential-score-tooltip">+{formatNumber(
+              potentialTopScores.find(score => score.id === scoreData.id).impact)
+            }</p>
+            }
+            <Tooltip id="potential-score-tooltip" place="bottom" style={{maxWidth: '400px'}}>
+              {t('score.card.tooltips.potentialScore')}
+            </Tooltip>
         </div>
-      ) : (
-      <Link className="name-wrapper" to={`/passes/${scoreData.id}`}>
-        <p className="score-desc-creator">{formatCreatorDisplay(scoreData.level)}</p>
-        <MarqueeText className="score-desc score-desc-song" as="p">
-          {scoreData.level.song}
-        </MarqueeText>
-        <MarqueeText className="score-exp score-exp-artist" as="p">
-          {scoreData.level.artist ?? 'Hidden level'}
-        </MarqueeText>
-      </Link>
-      )}
-      {scoreData.isWorldsFirst && (
-        <WorldsFirstFlag variant="clear" tooltipIndex={`${scoreData.id}-clear`} className="wf-badge" />
-      )}
-      {scoreData.isWorldsFirstPP && (
-        <WorldsFirstFlag variant="pp" tooltipIndex={`${scoreData.id}-pp`} className="wf-badge" />
-      )}
-      <div className="score-wrapper">
-          <p className="score-exp">{t('score.card.labels.score')}</p>
-          <p className="score-desc">{formatScore(scoreData.scoreV2)}</p>
-          {
-          topScores.find(score => score.id === scoreData.id) ?
-          <p className="score-impact">+{formatNumber(
-            topScores.find(score => score.id === scoreData.id).impact)
-          }</p> :
-          potentialTopScores.find(score => score.id === scoreData.id) &&
-           <p className="score-impact potential"
-           data-tooltip-id="potential-score-tooltip">+{formatNumber(
-            potentialTopScores.find(score => score.id === scoreData.id).impact)
-          }</p>
-          }
-          <Tooltip id="potential-score-tooltip" place="bottom" style={{maxWidth: '400px'}}>
-            {t('score.card.tooltips.potentialScore')}
-          </Tooltip>
-      </div>
-      <div className="acc-wrapper">
-        <div className="acc-wrapper-inner">
-          <p className="score-exp">{t('score.card.labels.accuracy')}</p>
-          <div className={`score-desc ${scoreData.accuracy == 1 ? 'pure-perfect' : ''}`}>{formatAccuracyRatio(scoreData.accuracy)}</div>
-          </div>
-          {scoreData.judgements && <Judgements judgements={scoreData.judgements} />}
-      </div>
+        <div className="acc-wrapper">
+          <div className="acc-wrapper-inner">
+            <p className="score-exp">{t('score.card.labels.accuracy')}</p>
+            <div className={`score-desc ${scoreData.accuracy == 1 ? 'pure-perfect' : ''}`}>{formatAccuracyRatio(scoreData.accuracy)}</div>
+            </div>
+            {scoreData.judgements && <Judgements judgements={scoreData.judgements} />}
+        </div>
 
-      <div className="speed-wrapper">
-          <p className="score-exp">{t('score.card.labels.speed')}</p>
-          <div className="score-desc">{clampFloat(scoreData.speed, 2)}×</div>
-      </div>
+        <div className="speed-wrapper">
+            <p className="score-exp">{t('score.card.labels.speed')}</p>
+            <div className="score-desc">{clampFloat(scoreData.speed, 2)}×</div>
+        </div>
 
-      {isPassCardMode && <PassFlags pass={scoreData} t={t} />}
+        {isPassCardMode && <PassFlags pass={scoreData} t={t} />}
 
-      {(formattedDate || (scoreData.videoLink && !isHiddenLevel)) && (
-        <div className="score-card__trailing">
-          {formattedDate && (
-            <time className="score-card__date" dateTime={scoreData.vidUploadTime}>
-              {formattedDate}
-            </time>
-          )}
-
-          <div className="vid-logo-wrapper">
-            {scoreData.videoLink && !isHiddenLevel && (
-              <a className="svg-fill" href={getPrimaryVideoLink(scoreData.videoLink)} target="_blank" rel="noreferrer" title={t('score.card.tooltips.watchVideo')}>
-                <VideoLinkIcon size="32px" url={scoreData.videoLink} />
-              </a>
+        {(formattedDate || (scoreData.videoLink && !isHiddenLevel)) && (
+          <div className="score-card__trailing">
+            {formattedDate && (
+              <time className="score-card__date" dateTime={scoreData.vidUploadTime}>
+                {formattedDate}
+              </time>
             )}
+
+            <div className="vid-logo-wrapper">
+              {scoreData.videoLink && !isHiddenLevel && (
+                <a className="svg-fill" href={getPrimaryVideoLink(scoreData.videoLink)} target="_blank" rel="noreferrer" title={t('score.card.tooltips.watchVideo')}>
+                  <VideoLinkIcon size="32px" url={scoreData.videoLink} />
+                </a>
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
 
       <span className="score-card__clear-id">#{scoreData.id}</span>
     </div>
